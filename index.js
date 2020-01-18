c818e6785997b045541cabb51018cdfd4f802b9c
Sean <cd3k@pop-os.localdomain>
2020-01-18 19:40:50 +0000

    included jqstring function in index

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const jQStringMaker = require('./qStringLib.js');
 module.exports = class JsonQL {
   constructor(schema) {
     this.schema = schema;
@@ -480,7 +479,7 @@ module.exports = class JsonQL {
   }
   
   jExString(db, table, jQString) {
-    let str = jQStringMaker(db, table, jQString);
+    let str = this.jQStringMaker(db, table, jQString);
     console.log(str);
     return str;
     // let column = this.jColString(db, table, jQString);
@@ -791,10 +790,6 @@ module.exports = class JsonQL {
     }
   }
 
-  keyIsQuery(key) {
-    return /^\$/.test(key); 
-  }
-
   // ▀█░█▀ █▀▀█ █░░ ░▀░ █▀▀▄ █▀▀█ ▀▀█▀▀ ░▀░ █▀▀█ █▀▀▄
   // ░█▄█░ █▄▄█ █░░ ▀█▀ █░░█ █▄▄█ ░░█░░ ▀█▀ █░░█ █░░█
   // ░░▀░░ ▀░░▀ ▀▀▀ ▀▀▀ ▀▀▀░ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀▀ ▀░░▀
@@ -847,4 +842,66 @@ module.exports = class JsonQL {
     }
   }
 
+  // ░░▀ █▀▀█ █▀▀ ▀▀█▀▀ █▀▀█ ░▀░ █▀▀▄ █▀▀▀ █▀▀
+  // ░░█ █░░█ ▀▀█ ░░█░░ █▄▄▀ ▀█▀ █░░█ █░▀█ ▀▀█
+  // █▄█ ▀▀▀█ ▀▀▀ ░░▀░░ ▀░▀▀ ▀▀▀ ▀░░▀ ▀▀▀▀ ▀▀▀
+
+  keyIsQuery(key) {
+    return /^\$/.test(key); 
+  }
+
+  jQString(db, table, str) {
+
+    let reg = /(\$\w+)|(\[\d\])|(\.\w+)|(\[\?[\w\s@#:;{},.!"£$%^&*()/?|`¬\-=+~]*\])/g
+
+    // let str = '$jsonForm[0].value[?Booking Month].label';
+
+    if(!str.startsWith('$')) {
+      console.log('json query strings must start with a $ sign');
+      return;
+    }
+
+    const matches = str.match(reg);
+
+    console.log(matches);
+
+    function which(string, prevString) {
+      if(/\$\w+/.test(string)) {
+        // 'name';
+        return `${db}.${table}.${string.slice(1)}`;
+      }
+      if(/\[\d\]/.test(string)) {
+        // 'index';
+        return `JSON_EXTRACT(${prevString}, "$${string}")`
+      }
+      if(/\.\w+/.test(string)) {
+        // 'target';
+        return `JSON_EXTRACT(${prevString}, "$${string}")`
+      }
+      if(/\[\?[\w\s@#:;{},.!"£$%^&*()/?|`¬\-=+~]*\]/.test(string)) {
+        // 'search';
+        string = string.slice(2, -1);
+        return `JSON_EXTRACT(${prevString}, CONCAT('$[', SUBSTR(JSON_SEARCH(${prevString}, 'all', '${string}'), 4, 1), ']'))`;
+      }
+    }
+
+    let name = /\$\w+/;
+    let index = /\[\d\]/;
+    let target = /\.\w+/;
+    let search = /\[\?[\w\s@#:;{},.!"£$%^&*()/?|`¬\-=+~]*\]/;
+
+    let result = matches.reduce((arr, match, i) => {
+
+      if(i === 0) {
+        return [...arr, which(match)];
+      }
+      return [...arr, which(match, arr[i-1])];
+
+    }, []);
+
+    console.log(result);
+
+    return result[result.length - 1];
+
+  }
 }
